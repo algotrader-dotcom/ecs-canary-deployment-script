0511939a26d14f466d6c1bb6f8aa0ccf48f30b93
Thuan Ng <ninhthuan82us@gmail.com>
2020-10-30 17:53:39 +0700

    Create ecs-canary.sh

---
+++ ecs-canary.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+### Required: 
+### 1 - AWS CLI V2: https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
+### 2 - Packages: git, jq, docker
+
+## Initialize & create ecr repo
+export AWS_DEFAULT_REGION=eu-west-1
+export ecs_repo='ecs-sample-app'
+
+aws ecr create-repository --repository-name $ecs_repo --region eu-west-1
+
+## Build docker image v1 & v2
+cd Docker/
+echo "Repo: "$ecs_repo
+sleep 5
+
+docker build -t $ecs_repo .
+docker tag $ecs_repo:latest 703043637716.dkr.ecr.eu-west-1.amazonaws.com/$ecs_repo:v1
+3aws ecr get-login --no-include-email --registry-ids 703043637716| bash
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 703043637716.dkr.ecr.eu-west-1.amazonaws.com/$ecs_repo
+
+docker push 703043637716.dkr.ecr.eu-west-1.amazonaws.com/$ecs_repo:v1
+
+cd ../
+
+git fetch && git checkout v2
+
+cd Docker/
+
+echo "Repo: "$ecs_repo
+sleep 5
+
+docker build -t $ecs_repo .
+docker tag $ecs_repo:latest 703043637716.dkr.ecr.eu-west-1.amazonaws.com/$ecs_repo:v2
+3aws ecr get-login --no-include-email --registry-ids 703043637716| bash
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 703043637716.dkr.ecr.eu-west-1.amazonaws.com/$ecs_repo
+
+docker push 703043637716.dkr.ecr.eu-west-1.amazonaws.com/$ecs_repo:v2
+
+cd ../
+
